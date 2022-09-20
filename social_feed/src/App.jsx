fba484b30e33a8d8f52394dc319d1587ab176af2
gregreust <greg.reust@gmail.com>
2022-09-19 21:45:26 -0500

    Added nav and WritePost component

---
+++ social_feed/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div id='root'>
+      <nav>
+        <h2>Social Feed</h2>
+      </nav>
+      <WritePost/>
+    </div>
+  );
+}
+
+export default App;
---
+++ social_feed/src/components/WritePost.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+const  WritePost = (props) => {
+    return (  
+        <form>
+            <label>Name</label>
+            <input/>
+            <label>Write post</label>
+            <input/>
+        </form>
+    );
+}
+ 
+export default WritePost;
