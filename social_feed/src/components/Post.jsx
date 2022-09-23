bed87d595afc4a6a6253928e22e03fa03d4579b6
gregreust <greg.reust@gmail.com>
2022-09-23 10:40:57 -0500

    Like and Dislike buttons change color on click. Added background colors

--- social_feed/src/components/Post.jsx
+++ social_feed/src/components/Post.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Post = ({postObject}) => {
+
+    const [likeButtonClass, setLikeButtonClass] = useState("inactive");
+    const [dislikeButtonClass, setDislikeButtonClass] = useState("inactive");
+
+    function handleLikeClick() {
+        if (likeButtonClass === "inactive"){
+            setLikeButtonClass("liked");
+            if (dislikeButtonClass === "disliked"){
+                setDislikeButtonClass("inactive");
+            }
+        } else {
+            setLikeButtonClass("inactive");
+        }
+    }
+
+    function handleDislikeClick(){
+        if (dislikeButtonClass === "inactive"){
+            setDislikeButtonClass("disliked");
+            if (likeButtonClass === "liked"){
+                setLikeButtonClass("inactive");
+            }
+        } else {
+            setDislikeButtonClass("inactive");
+        }
+    }
+
      return (
         <div class="post">
             <div class="name">{postObject.name}</div>
             <div class="body">{postObject.body}</div>
             <div class="date">{postObject.date}</div>
             <div class="time">{postObject.time}</div>
-            <button class="reaction" id="like">Like</button>
-            <button class="reaction" id="dislike">Dislike</button>
+            <button class={likeButtonClass} onClick={handleLikeClick}>Like</button>
+            <button class={dislikeButtonClass} onClick={handleDislikeClick}>Dislike</button>
         </div>
     );
 }
